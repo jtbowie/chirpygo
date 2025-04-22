bf2897638d36c81ab9f0ab9c3cde00f28492fcab
Jon Bowie <jon.bowie@gmail.com>
2025-04-22 09:05:12 -0400

    added /healthz endpoint, and /app/ rewrite

--- main.go
+++ main.go
@@ -2,12 +2,31 @@ package main
 
 import "net/http"
 
+func httpHandler(httpWriter http.ResponseWriter, httpRequest *http.Request) {
+	httpWriter.Header().Set("Content-type", "text/plain; charset=utf-8")
+	httpWriter.WriteHeader(200)
+	_, err := httpWriter.Write([]byte("OK"))
+	if err != nil {
+		// TODO: Implement Error message/handling
+		// This should never fail, but it's worth handling the error case"
+		return
+	}
+
+}
+
 func main() {
 	myServeMux := http.NewServeMux()
 	var myHTTPServer http.Server
 
-	myServeMux.Handle("/", http.FileServer(http.Dir(".")))
+	defer myHTTPServer.Close()
+
+	httpFileServer := http.FileServer(http.Dir("."))
+	myServeMux.HandleFunc("/healthz", httpHandler)
+	myServeMux.Handle("/app/", http.StripPrefix("/app", httpFileServer))
 	myHTTPServer.Handler = myServeMux
 	myHTTPServer.Addr = ":8080"
-	myHTTPServer.ListenAndServe()
+	err := myHTTPServer.ListenAndServe()
+	if err != nil {
+		return
+	}
 }
